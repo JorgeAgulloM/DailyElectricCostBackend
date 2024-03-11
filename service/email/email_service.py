80a573cbfdaa9aee37569fd9dc2aafa037db057f
Jorge1 <agullojorge@gmail.com>
2024-03-11 20:36:46 +0100

    Revert "Created srv model"
    This reverts commit 310fe14d1fa24dc517cb422136b255affd9912c2.

--- service/email/email_service.py
+++
@@ -1,9 +0,0 @@
-### EMAIL SERVICE ###
-from data.db.email.email_repository import search_subscripters, insert_subscription
-from service.email.models.subscripter import SubscriptorSrv, mapper_data_to_service, mapper_service_to_data
-
-def get_subscriptors():
-    return search_subscripters()
-
-def insert_subscriptor(subscriptor: SubscriptorSrv):
-    return insert_subscription(mapper_service_to_data(subscriptor))
