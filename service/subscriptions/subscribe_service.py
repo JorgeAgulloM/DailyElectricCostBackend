9c6bd49336aa596b58dae58ee6587f60f5867dcc
Jorge1 <agullojorge@gmail.com>
2024-03-11 20:44:14 +0100

    Changed names

--- service/subscriptions/subscribe_service.py
+++ service/subscriptions/subscribe_service.py
@@ -1,6 +1,6 @@
 ### EMAIL SERVICE ###
 from data.db.subscribes.subscribe_repository import search_subscripters, insert_subscription
-from service.email.models.subscripter import SubscriptorSrv, mapper_data_to_service, mapper_service_to_data
+from service.subscriptions.models.subscripter import SubscriptorSrv, mapper_data_to_service, mapper_service_to_data
 
 def get_subscriptors():
     return search_subscripters()
