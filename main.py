418b2ca3529189478683af7ca5e2404e49e8c58c
Jorge1 <agullojorge@gmail.com>
2024-03-12 21:09:38 +0100

    Added point cero

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.subscribers.routes import api_subscribers
 
 app = FastAPI()
+
 app.include_router(api_subscribers.router)
+
+@app.get('/')
+def init():
+    return {'message': 'Start FastApi'}
 
 @app.get("/ping")
 def read_ping():
