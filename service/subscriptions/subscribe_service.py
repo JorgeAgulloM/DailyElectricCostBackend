375f58c0ae73a51ead7f8927d46de7de047d8877
Jorge1 <agullojorge@gmail.com>
2024-03-12 15:13:44 +0100

    Finish insert_subscriber

--- service/subscriptions/subscribe_service.py
+++ service/subscriptions/subscribe_service.py
@@ -1,21 +1,48 @@
 ### EMAIL SERVICE ###
-from data.db.subscribes.subscribe_repository import search_subscripters, insert_subscription
-from service.subscriptions.models.subscripter import SubscriptorSrv, mapper_service_to_data
+from datetime import datetime
+from bson import ObjectId
+
+from data.db.subscribes.subscribe_repository import search_subscribers, search_subscriber, insert, activate
+from service.subscriptions.models.subscripter import SubscriberSrv, mapper_service_to_data
 from data.email.email_repository import send_email_verification 
 
 
-def get_subscriptors():
-    return search_subscripters()
+def get_subscribers():
+    return search_subscribers()
 
 
-def insert_subscriptor(subscriptor: SubscriptorSrv):
+def insert_subscriber(subscriptor: SubscriberSrv):
+    
+    # Search if email is suscripted
+    email = subscriptor.email
+    subscriptor_exist = search_subscriber('email', email)
+
+    if subscriptor_exist:
+        if subscriptor.activated:
+            return {'error': 'Email suscripted'}
+        else:
+            return {'error': 'Email suscribed but not activated'}
+    
+    # Send and otain de activation code
+    code = send_email_verification(email)
+        
     # Insera al subscriptor en la DB
-    result = insert_subscription(mapper_service_to_data(subscriptor))
+    edit_subscriber = subscriptor
+    edit_subscriber.activation_code = code
+    insert(mapper_service_to_data(edit_subscriber))
     
-    # Si no hay error en la inserción, se genera y envia el correo de verificación.
-    if 'error' not in result:
-        email = subscriptor.email
-        return send_email_verification(email)
-    
-    return result
 
+def activate_subscriber(code: str):
+    try:
+        subscriber = search_subscriber('code', code)
+    except Exception as e:
+        return {'error': f'When getting user. Error: {e}'}
+    
+    subscriber.date_activated = str(datetime.now())
+    subscriber.activated = True
+    filter_id = {'_id': ObjectId(subscriber.id)}
+    
+    try:
+        return activate(filter_id, subscriber)
+    except Exception as e:
+        return {'error': f'When activating user. Error: {e}'}
