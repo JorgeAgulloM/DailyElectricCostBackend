080e594cc95440df03bbbccc0f2dc5aba5fe8e7f
Jorge1 <agullojorge@gmail.com>
2024-03-12 20:40:08 +0100

    Fixed insert_subsriber

--- service/subscriptions/subscribe_service.py
+++ service/subscriptions/subscribe_service.py
@@ -11,14 +11,15 @@ def get_subscribers():
     return search_subscribers()
 
 
-def insert_subscriber(subscriptor: SubscriberSrv):
+def insert_subscriber(email: str):
+    new_subscriber = SubscriberSrv(email = email)
     
     # Search if email is suscripted
-    email = subscriptor.email
+    email = new_subscriber.email
     subscriptor_exist = search_subscriber('email', email)
 
     if subscriptor_exist:
-        if subscriptor.activated:
+        if new_subscriber.activated:
             return {'error': 'Email suscripted'}
         else:
             return {'error': 'Email suscribed but not activated'}
@@ -27,9 +28,9 @@ def insert_subscriber(subscriptor: SubscriberSrv):
     code = send_email_verification(email)
         
     # Insera al subscriptor en la DB
-    edit_subscriber = subscriptor
-    edit_subscriber.activation_code = code
-    insert(mapper_service_to_data(edit_subscriber))
+    new_subscriber.activation_code = code
+    insert(mapper_service_to_data(new_subscriber))
+    return {'message': 'The email has been suscribed'}
     
 
 def activate_subscriber(code: str):
