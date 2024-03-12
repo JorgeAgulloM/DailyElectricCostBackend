436da73539d639944a5ab3629f6eb4e8c1f99143
Jorge1 <agullojorge@gmail.com>
2024-03-12 20:39:11 +0100

    Testing alternative email sender

--- data/email/email_repository.py
+++ data/email/email_repository.py
@@ -1,5 +1,6 @@
 ### Email Repository ###
 from data.email.email_service import send
+from data.email.email_service_alt import send_email
 
 def send_email_verification(recipient: str) -> str:
-    return send(recipient)
+    return send_email(recipient)
