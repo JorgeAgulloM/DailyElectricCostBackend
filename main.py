f7cf820dc0be75ee1057c0abfda88948b1d07bdd
Jorge1 <agullojorge@gmail.com>
2024-03-14 21:29:10 +0100

    Implemented new api

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
 from api.subscribers.routes import api_subscribers
-
+from api.contact.routes import api_contact
 app = FastAPI()
 
 # Make public assets directory
 app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 app.include_router(api_subscribers.router)
+app.include_router(api_contact.router)
 
 @app.get('/')
 def init():
