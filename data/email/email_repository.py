63239d915f59de1b9e6e380e84af88874864a413
Jorge1 <agullojorge@gmail.com>
2024-03-14 21:27:36 +0100

    Implement new send function

--- data/email/email_repository.py
+++ data/email/email_repository.py
@@ -1,5 +1,9 @@
 ### Email Repository ###
-from data.email.email_service import send
+from data.email.email_service import send, send_contact
+from data.email.models.contact import ContactDB
 
 def send_email_verification(recipient: str) -> str:
     return send(recipient)
+
+def send_email_contact_developer(contact: ContactDB):
+    send_contact(contact)
