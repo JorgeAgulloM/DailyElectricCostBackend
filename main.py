4d016a10d39affaa84ddc8549df4134656b67d07
Jorge1 <agullojorge@gmail.com>
2024-03-15 09:03:49 +0100

    Added CORS origin and middleware

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 from api.subscribers.routes import api_subscribers
 from api.contact.routes import api_contact
@@ -10,18 +11,38 @@ app = FastAPI()
 # Make public assets directory
 app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
+origins = [
+    'https://dailyelectriccost.web.app',
+    'http://localhost:8080',
+    'http://localhost:4200'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True, 
+    allow_methods=['*'], 
+    allow_headers=['*']
+)
+
 app.include_router(api_subscribers.router)
 app.include_router(api_contact.router)
 
+
 @app.get('/')
-def init():
-    return {'message': 'Start FastApi'}
+def read_init():
+    return {'message': 'Start DailyElectricCost Backend with Python and FastApi'}
+
 
 @app.get("/ping")
 def read_ping():
-    return {'response': "pong"}
+    return {'message': "pong"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.middleware('http')
+async def check_cors(request: Request, call_next):
+    print(request.headers)
+    if request.headers.get('origin') not in origins:
+        return JSONResponse(content={'error': 'Access denied'}, status_code=status.HTTP_403_FORBIDDEN)
+    response = await call_next(request)
+    return response
