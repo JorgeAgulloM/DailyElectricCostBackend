996fbc507fd2bee0bda9daa50ee47c4fd8ffdb43
Jorge1 <agullojorge@gmail.com>
2024-03-16 17:10:15 +0100

    Extracted activation/deactivation subscribers

--- api/subscribers/routes/api_subscribers.py
+++ api/subscribers/routes/api_subscribers.py
@@ -1,6 +1,6 @@
 ### Subscribers API ###
 
-from fastapi import FastAPI, APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from bson import ObjectId
 
 from api.subscribers.models.subscripter import SubscriberApi, mapper_api_to_service
@@ -11,8 +11,6 @@ router = APIRouter(
     tags=['subscribers'], 
     responses = {404: {'message': 'No encontrado'}}
 )
-
-publicApp = FastAPI()
 
 #####################################################################################################################
 ########################################### API CRUD Susbscripters ##################################################
@@ -35,17 +33,3 @@ async def get_subscriber_list():
 @router.post('/', status_code=status.HTTP_201_CREATED)
 async def insert_new_subscriber(subscriber: SubscriberApi):
     return insert_subscriber(mapper_api_to_service(subscriber))
-
-
-""" Activate Suscriptor """
-@publicApp.put('/activated_subscriber/{code}', status_code=status.HTTP_200_OK)
-async def go_to_activate_subscriber(code: str):
-    activate_subscriber(code)
-    return {'message': 'Email subscription has beenn activated'}
-
-
-""" Cancel Subscription """
-@router.put('/cancel_subscriber/{email}', status_code=status.HTTP_200_OK)
-async def cancel_subscription(email: str):
-    return
-
