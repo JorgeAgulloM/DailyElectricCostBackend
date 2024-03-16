87233845ba4825134a591be01b5b531125fea3be
Jorge1 <agullojorge@gmail.com>
2024-03-16 17:10:38 +0100

    Created public api for subscriptions actvie/deactive

---
+++ api/subscribers/routes/api_subscribers_public.py
@@ -0,0 +1,28 @@
+### Public API ###
+from fastapi import APIRouter, status
+
+from service.subscriptions.subscribe_service import activate_subscriber
+
+router = APIRouter(
+    prefix="/public/subscribers",
+    tags=['public', 'subscribers'], 
+    responses = {404: {'message': 'No encontrado'}}
+)
+
+""" Public Subscribes Ping """
+@router.get("/ping")
+def read_ping():
+    return {'message': "subscribers pong"}
+
+
+""" Activate Suscriptor """
+@router.put('/activated_subscriber/{code}', status_code=status.HTTP_200_OK)
+async def go_to_activate_subscriber(code: str):
+    activate_subscriber(code)
+    return {'message': 'Email subscription has beenn activated'}
+
+
+""" Cancel Subscription """
+@router.put('/cancel_subscriber/{code}', status_code=status.HTTP_200_OK)
+async def cancel_subscription(code: str):
+    return
